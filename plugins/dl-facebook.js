fa112de63253b7cc6807da4c1b5c146e8bd2eeac
CUTE-BACHA🙂❤️ <158988215+Umar4t7@users.noreply.github.com>
2024-03-09 14:50:27 +0500

    LOVER BOY MD

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -4,18 +4,18 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
  if (!args[0] && m.quoted && m.quoted.text) {
   args[0] = m.quoted.text;
 }
-if (!args[0] && !m.quoted) throw `✳️ Provide a Facebook link first \n📌 Example:\n*${usedPrefix + command}* https://www.facebook.com/...`
+if (!args[0] && !m.quoted) throw `💢 Please give me a valid Facebook video Url \n📌 Example:\n*${usedPrefix + command}* https://www.facebook.com/...`
     m.react(rwait)
    try {
     let result = await fg.fbdl(args[0]);
     let tex = `
-┌─⊷ *⚪PRINCE FBDL⚪*
+┌─⊷ *⚪LOVER-BOY-MD FB⚪*
 ▢ *Title:* ${result.title}
 └───────────`;
     conn.sendFile(m.chat, result.videoUrl, 'fb.mp4', tex, m);
     m.react(done);
   } catch (error) {
- 	m.reply('❎Enter a proper facebook link ')
+ 	m.reply('Sorry I Can't Find Anything☹️ ')
  	} 
 }
 handler.help = ['facebook'].map(v => v + ' <url>')
