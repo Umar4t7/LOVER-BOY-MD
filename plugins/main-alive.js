38c38428878ecc84142219c22b3b3ba8b89e45e4
CUTE-BACHA🙂❤️ <158988215+Umar4t7@users.noreply.github.com>
2024-03-10 08:22:04 +0500

    LOVER BOY MD

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -3,9 +3,9 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "https://github.com/PRINCE-GDS/THE-PRINCE-BOT/raw/main/Assets/mp3/Audio5.mp3"
-    let url = "https://github.com/PRINCE-GDS/THE-PRINCE-BOT"
+    let url = "https://github.com/Umar4t7/LOVER-BOY-MD"
     let murl = "https://youtu.be/DibiLc17dh0?si=xp9bQ-_frEyDB1-i"
-    let img = "https://i.imgur.com/oX4ES3B.jpeg"
+    let img = "https://telegra.ph/file/7f281da946ecf82d2e306.jpg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -19,8 +19,8 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "THE-PRINCE-BOT IS ALIVE",
-          body: "PRINCE BOT",
+          title: "LOVER BOY MD IS ALIVE NOW TYPE MENU TO GET ALL COMMANDS LIST🤗♥️",
+          body: "LOVER BOY MD",
           thumbnailUrl: img,
           sourceUrl: 'https://chat.whatsapp.com/Jo5bmHMAlZpEIp75mKbwxP',
           mediaType: 1,
